6a077749470b2586b82ee3438555dff5847e02b2
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-04-16 17:53:03 +0400

    create sendNearToWriter method //send 3 near to the writer   // if the articles of the writers have minum views count(5), then send 3 NEAR to these writers' balance

--- src/artenc/assembly/index.ts
+++ src/artenc/assembly/index.ts
@@ -1,31 +1,70 @@
-import { context, ContractPromiseBatch, PersistentMap, PersistentSet, PersistentVector, storage, u128 } from "near-sdk-as";
-import { Article } from "./model";
-
+import { context, ContractPromiseBatch, PersistentSet, u128 } from "near-sdk-as";
+import { Article, generalArticle } from "./model";
+import { checkData, checkDonation, checkOwner, findDonateableWriter,  returnMetaArticle,  useArticle } from "./utils";
 
 
 @nearBindgen
 export class Contract {
-  owner: string = context.sender;
-  // add  new article to the contract
-  add(url: string, title: string): string {
-    assert(url.length > 0, "url length must greater than zero");
-    assert(title.length > 0, "title length must greater than zero");
-    // Articles.push(new Article(url,title, context.predecessor));
-    //articlesArr.push(new Article(url,title, context.predecessor));
-    const article = new Article(url, title, context.predecessor);
-    Articles.add(article);
-    if (Articles.has(article)) {
-      return "Project is added successfully";
-    } else {
-      return "this article already added"
-    }
+    //owner of the contract
+  private CONTRACT_OWNER: string;
+  //minumum amount what required for adding a new article to the BLOCKCHAIN 
+  private MIN_FEE: u128 = u128.from("1000000000000000000000000"); //=1 NEAR
 
+  //initialize the project and define the owner of the project
+  //this owner is used  later to run administrative commands
+
+  constructor(owner: string) {
+    this.CONTRACT_OWNER = owner;
+  }
+
+
+  // add  new article to the contract
+  // first check if user added required NEAR to context
+  // check whether the data added
+  //then create new Article object
+  // check if the article object is already added or not. If article is not in the storage, then
+  // add the new article to the storage
+  add(url: string, title: string): string {
+    const userDonation = context.attachedDeposit;
+    checkDonation(userDonation, this.MIN_FEE);
+    checkData(url, title);
+    const article = new Article(url, title, context.sender);
+    if (Articles.has(article)) {
+      return "this article already added";
+    }
+    else {
+      Articles.add(article);
+      if (Articles.has(article)) {
+        return "Project is added successfully";
+      } else {
+        return "something went wrong"
+      }
+    }
+  }
+
+  //donate the writer
+  // if the articles of the writers have minum views count(5), then send 3 NEAR to these writers' balance 
+  sendNearToWriter(): string {
+      //check whether the sender of the transaction is the owner of the contract
+      checkOwner(context.sender, this.CONTRACT_OWNER);
+      //find all writers who have five viewed articles
+    let writers = findDonateableWriter();
+    
+    if (writers.length == 0) {
+      return "there are no donateable writers to donate"
+    }
+    else {
+      for (let i = 0; i < writers.length; i++) {
+        const to_writer = ContractPromiseBatch.create(writers[i]);
+        const donationAmount: u128 = u128.from("3000000000000000000000000");
+        to_writer.transfer(donationAmount);
+      }
+      return `you donated these/this ${writers.toString()} writers/writer`
+    }
   }
 
 
 
-}
-
 
 //define Articles container on the storage
-let Articles = new PersistentSet<Article>("a");
+export let Articles = new PersistentSet<Article>("a");
