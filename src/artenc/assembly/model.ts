bafab83991d85389ca8e5827b4e089e997670935
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-04-16 17:44:21 +0400

    1. create main article class 2. create specific article class that has not url property

--- src/artenc/assembly/model.ts
+++ src/artenc/assembly/model.ts
@@ -1,19 +1,33 @@
 
+// main article class
 @nearBindgen
 export class Article{
   url:string;
   title:string;
   sender: string;
-
+  static lastCount:number =0;
+  count:number;
   constructor(url:string, title:string, sender:string){
       this.url =url;
       this.sender = sender;
       this.title = title;
-      
+      this.count =0;
   }
-
-  // private createRandomId():string{
-  //   const id:string = Math.random().toString(36).slice(3);
-  //   return id;
-  // }
+  //increase count property value when the class object created
+  // every time when the same writer adds a new article, this property is increased
+    increaseCount():void{
+       this.count = ++Article.lastCount;
+  }
 }
+
+// article class without url property
+@nearBindgen
+export class generalArticle{
+  title:string
+  sender:string
+  constructor(title:string, sender:string){
+      this.title=title;
+      this.sender = sender;
+  }
+}
+
